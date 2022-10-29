72918f4f49643014efefa6c64b4d9fdcb8c9d50e
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-10-29 13:09:18 -0300

    'deploy'

--- src/services/sendEmails/EmailExportContactFormService.ts
+++ src/services/sendEmails/EmailExportContactFormService.ts
@@ -8,7 +8,7 @@ class EmailExportContactFormService {
             port: 587,
             auth: {
                 user: "contato@builderseunegocioonline.com",
-                pass: "3"
+                pass: "2222222222"
             }
         })
 
